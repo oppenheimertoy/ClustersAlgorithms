20e34e7396d8d4ffc61e8dc2c7d2929afc1e140a
oppenheimertoy <titaninus4590@yandex.ru>
2021-05-07 01:23:07 +0300

    Interface v1.0

---
+++ Interface.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Oval.h"
+#include "Point.h"
+#include "Field.h"
+#include "DBSCAN.h"
+#include "WAVE.h"
+#include "HIERARCHICAL.h"
+#include "Poisk.h"
+#include<iostream>
+#include <fstream>
+
+class Interface {
+private:
+	Field pole;
+	Poisk search;
+public:
+	Interface()
+};
+
